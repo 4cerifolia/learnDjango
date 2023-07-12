238f7d60735042d6fa53b8b56d50d4b923370951
4cerifolia <603898428@qq.com>
2023-07-12 15:32:44 +0800

    django

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+    return HttpResponse("hello i am login web")
